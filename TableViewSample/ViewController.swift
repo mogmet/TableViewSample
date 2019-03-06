de90f50583d834a7ca1914d7950efc8541392c7b
mogmet <ito.daiki@willgate.co.jp>
2019-03-06 16:07:18 +0900

    Initial Commit

---
+++ TableViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewSample
+//
+//  Created by 伊藤 大樹 on 2019/03/06.
+//  Copyright © 2019 Willgate,Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
